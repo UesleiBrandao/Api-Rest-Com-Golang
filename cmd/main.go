fda0374617aa164d59e12dc71e06d37d575a3579
UesleiBrandao <uesleisouzabrandao@gmail.com>
2026-01-18 21:11:06 -0300

    Primeiro commit: Estrutura da API Rest com conexão Postgres

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	db_conection, err := db.ConnectDB()
+	if err != nil {
+
+		panic(err)
+	}
+
+	ProductRepository := repository.NewProductRepository(db_conection)
+
+	ProductUseCase := usecase.NewProductUseCase(ProductRepository)
+	ProductController := controller.NewProductController(ProductUseCase)
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+	server.Run(":8000")
+}
